c9b2ec0e12655c68958eb7744262a6549258be24
ThanhSang <nguyenthanhsang160405@gmail.com>
2025-07-30 12:37:16 +0700

    fix : vnpay

--- src/app/(user)/pageHandleGoogle/page.tsx
+++ src/app/(user)/pageHandleGoogle/page.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense } from "react";
-import ClientPageHandleGoogle from "./component/pageHandleGoogle";
+import PageHandleGoogle from "./component/pageHandleGoogle";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Đang xử lý đăng nhập bằng Google...</div>}>
-      <ClientPageHandleGoogle />
+      <PageHandleGoogle />
     </Suspense>
   );
 }
